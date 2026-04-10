04f1b44f9c7249121f837befa867b16d055ccfd1
nameA42 <Benepratt1@gmail.com>
2026-04-09 20:35:08 -0700

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorldSection2
+{
+    public class HelloWorld
+    {
+        private int a = 0;
+
+        public HelloWorld()
+            {
+            a = 1;
+            }
+        public static string helloWorlder(int numWorlds)
+        {
+            if (numWorlds <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Hey Buddy, make it bigger than 0 ): You did this IDIOT" + numWorlds, "numWorlds");
+            }
+            string worldMsg = string.Empty;
+            for (int i = 0; i < numWorlds; i++)
+            {
+                worldMsg += "World! ";
+            }
+            return worldMsg;
+        }
+
+    }
+}
